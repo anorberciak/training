b71428e28b59b3f217652f979149555343bb3f2e
anorberciak <a.norberciak@gmail.com>
2018-07-01 12:11:57 +0200

    Loop adding

--- hello.cpp
+++ hello.cpp
@@ -6,6 +6,9 @@ using namespace std;
 int main()
 {
     cout<<"Hello World!"<<endl;
-    cout<<"Hello Coders School"<<endl;
+    for(int i =0;i<10;i++)
+    {
+    cout<<i<<endl;
+    }
     return 0;
 }
